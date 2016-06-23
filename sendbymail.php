3deca0e943bfc4576cff9a6b36ada7b38fe19fae
Diego Armando Castellanos Sabogal <castsabproyectos@gmail.com>
2016-06-23 17:13:49 -0500

    first commit

--- sendbymail.php
+++ sendbymail.php
@@ -1,16 +1,11 @@
 <?php
 error_reporting(E_ALL);
 
-echo 'paso 1';
 
-include_once('PHPMailer/_lib/class.phpmailer.php');
-include_once('PHPMailer/_lib/class.smtp.php');
-
-echo 'paso 2';
+include_once('PHPMailer/class.phpmailer.php');
+include_once('PHPMailer/class.smtp.php');
 
 if (isset($_POST['email'])) {
-    
-    echo 'paso 3';
     
     if (!isset($_POST['name']) || !isset($_POST['country']) || !isset($_POST['email'])) {
         echo'<script type="text/javascript">
@@ -19,8 +14,6 @@ if (isset($_POST['email'])) {
              </script>';
         return false;
     }
-    
-    echo 'paso 4';
     
     $mail = new PHPMailer();
     $mail->IsSMTP();
@@ -31,49 +24,31 @@ if (isset($_POST['email'])) {
     $mail->Username ='castsabproyectos@gmail.com';
     $mail->Password = 'castsab04072012'; 
     
-    echo 'paso 5';
-    
     $message = "";
     $to = "dcastellanos@ciercentro.edu.co";
-    $subject = 'Comentario - Virtual Educa 2017.';
+    $subject = 'Sugerencias - Virtual Educa 2017.';
     
-    $message = "Detalles del formulario de contacto:\n\n";
-    $message .= "Nombre: " . $_POST['name'] . "\n";
-    $message .= "Pais: " . $_POST['country'] . "\n";
-    $message .= "E-mail: " . $_POST['email'] . "\n";
-    $message .= "Sugerencias: " . $_POST['messageSuggestion'] . "\n";
-    $message .= "Opción para el Virtual Educa 2017: " . $_POST['messageOption'] . "\n\n";
+    $message = "Detalles del formulario de contacto: <br>";
+    $message .= "Nombre: " . $_POST['name'] . "<br>";
+    $message .= "Pais: " . $_POST['country'] . "<br>";
+    $message .= "E-mail: " . $_POST['email'] . "<br>";
+    $message .= "Sugerencias: " . $_POST['messageSuggestion'] . "<br>";
+    $message .= "Opción para el Virtual Educa 2017: " . $_POST['messageOption'] . "<br>";
     
-    echo 'paso 6';
-    
-    //Agregar destinatario
     $mail->AddAddress($to);
     $mail->Subject = $subject;
     $mail->Body = $message;
-    //Para adjuntar archivo
-    //$mail->AddAttachment($archivo['tmp_name'], $archivo['name']);
     $mail->MsgHTML($message);
     
-    echo 'paso 7';
-
     if($mail->Send()){
-        
-        echo 'ErrorInfo -> '.$mail->ErrorInfo();
-        
         echo '<script type="text/javascript">
                 alert("Enviado Correctamente");
-                window.history.back();
+                window.location = "index.html"
              </script>';
     }else{
-        
-        echo 'ErrorInfo -> '.$mail->ErrorInfo();
-        
         echo '<script type="text/javascript">
                 alert("NO ENVIADO, intentar de nuevo");
-                window.history.back();
              </script>';
     }
-    
-    echo 'paso 9';
 }
 ?>
